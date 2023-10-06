9255f60b498ddaaa5c6b37813eb9beb37491aa36
Sam DeCook <45473144+sam-decook@users.noreply.github.com>
2023-10-05 21:05:25 -0400

    small refactor

--- computer-networks/http-server-client/client.py
+++ computer-networks/http-server-client/client.py
@@ -1,16 +1,17 @@
 from socket import *
 import sys
 
+# Command format:
+# client.py address port file
+
 serverName = sys.argv[1]
 serverPort = int(sys.argv[2])
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-request = "GET /" + sys.argv[3] + " HTTP/1.1\r\n"
-host = "Host: cedarville.edu\r\n\r\n"
-clientSocket.send(request.encode())
-clientSocket.send(host.encode())
+clientSocket.send(("GET /" + sys.argv[3] + " HTTP/1.1\r\n").encode())
+clientSocket.send(("Host: cedarville.edu\r\n\r\n").encode())
 
 response = clientSocket.recv(1024 * 8)
 print("From server:" + response.decode())
